b75930824470ef43a657652f207645d17439a52e
smaa97 <30627254+smaa97@users.noreply.github.com>
2017-08-14 11:15:36 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/Article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/Article-TWO', function (req, res) {
-  res.send('Article Two Requested & Will be served HERE');
+  res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 app.get('/Article-THREE', function (req, res) {
-  res.send('Article THREE Requested & Will be served HERE');
+  res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
